d916ac57dc8abc1dba4ec6514cc2d0f6b0945972
Bohdan Chupika <boroda4436@users.noreply.github.com>
2023-11-13 13:03:31 +0200

    remove encoding config from checkstyle plugin

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,44 @@
 package core.basesyntax;
 
 public class RobotRoute {
-    public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+    public void moveRobot(Robot robot, int toX, int toY) { // RobotRoute class where the main logic for navigation is implemented
+
+            // Helper method to turn the robot to face the target position
+            private void turnToTargetDirection(Robot robot, int toX, int toY) {
+                // Determine the direction based on target position
+                if (toX > robot.getX()) {
+                    while (robot.getDirection() != Direction.RIGHT) {
+                        robot.turnRight();
+                    }
+                } else if (toX < robot.getX()) {
+                    while (robot.getDirection() != Direction.LEFT) {
+                        robot.turnRight();
+                    }
+                } else if (toY > robot.getY()) {
+                    while (robot.getDirection() != Direction.UP) {
+                        robot.turnRight();
+                    }
+                } else if (toY < robot.getY()) {
+                    while (robot.getDirection() != Direction.DOWN) {
+                        robot.turnRight();
+                    }
+                }
+            }
+
+            // Helper method to move the robot step by step towards the target
+            private void moveRobotStep(Robot robot, int toX, int toY) {
+                while (robot.getX() != toX || robot.getY() != toY) {
+                    turnToTargetDirection(robot, toX, toY);
+                    robot.stepForward();
+                }
+            }
+
+            // Main method to move the robot to the target position
+            public void moveRobot(Robot robot, int toX, int toY) {
+                moveRobotStep(robot, toX, toY);
+            }
+        }
+
+        //write your solution here
     }
 }
