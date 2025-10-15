bd1cceec9e1daabf7b6413e0bd257b73bc0b6272
Biletska Liudmyla <bileckaludmila74@gmail.com>
2025-10-15 22:06:11 +0200

    modified Robot.java and RobotRoute.java

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,44 +1,38 @@
 package core.basesyntax;
 
 public class RobotRoute {
-    public void moveRobot(Robot robot, int toX, int toY) { // RobotRoute class where the main logic for navigation is implemented
+    public void moveRobot(Robot robot, int toX, int toY) {
+        int currentX = robot.getX();
+        int currentY = robot.getY();
 
-            // Helper method to turn the robot to face the target position
-            private void turnToTargetDirection(Robot robot, int toX, int toY) {
-                // Determine the direction based on target position
-                if (toX > robot.getX()) {
-                    while (robot.getDirection() != Direction.RIGHT) {
-                        robot.turnRight();
-                    }
-                } else if (toX < robot.getX()) {
-                    while (robot.getDirection() != Direction.LEFT) {
-                        robot.turnRight();
-                    }
-                } else if (toY > robot.getY()) {
-                    while (robot.getDirection() != Direction.UP) {
-                        robot.turnRight();
-                    }
-                } else if (toY < robot.getY()) {
-                    while (robot.getDirection() != Direction.DOWN) {
-                        robot.turnRight();
-                    }
-                }
+        if (currentX < toX) {
+            turnTo(robot, Direction.RIGHT);
+            while (robot.getX() < toX) {
+                robot.stepForward();
             }
-
-            // Helper method to move the robot step by step towards the target
-            private void moveRobotStep(Robot robot, int toX, int toY) {
-                while (robot.getX() != toX || robot.getY() != toY) {
-                    turnToTargetDirection(robot, toX, toY);
-                    robot.stepForward();
-                }
-            }
-
-            // Main method to move the robot to the target position
-            public void moveRobot(Robot robot, int toX, int toY) {
-                moveRobotStep(robot, toX, toY);
+        } else if (currentX > toX) {
+            turnTo(robot, Direction.LEFT);
+            while (robot.getX() > toX) {
+                robot.stepForward();
             }
         }
+        if (currentY < toY) {
 
-        //write your solution here
+            turnTo(robot, Direction.UP);
+            while (robot.getY() < toY) {
+                robot.stepForward();
+            }
+        } else if (currentY > toY) {
+            turnTo(robot, Direction.DOWN);
+            while (robot.getY() > toY) {
+                robot.stepForward();
+            }
+        }
+    }
+
+    private void turnTo(Robot robot, Direction targetDirection) {
+        while (robot.getDirection() != targetDirection) {
+            robot.turnRight();
+        }
     }
 }
